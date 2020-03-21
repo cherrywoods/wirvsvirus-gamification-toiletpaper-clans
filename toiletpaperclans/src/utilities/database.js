a554dff3c56f4438ba038487db0d3006def1f8e7
David Boetius <david.boetius@uni-konstanz.de>
2020-03-21 16:47:31 +0100

    adding register listener methods for desinfectant and toiletpaper for your team

--- toiletpaperclans/src/utilities/database.js
+++ toiletpaperclans/src/utilities/database.js
@@ -16,9 +16,38 @@ export function initFirebase() {
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   }
-
+  
   // test console log
   firebase.database().ref('Team/').once('value', function (snapshot) {
     console.log(snapshot.val())
+    registerDesinfectantListener((desi) => {console.log("desinfectant: " + desi)}, "-M2xiPYYzZTU5u9yy3Fa");
+    registerToiletpaperListener((tp) => {console.log("toiletpaper: " + tp)}, "-M2xiPYYzZTU5u9yy3Fa");
   });
+}
+
+export function registerDesinfectantListener(listener, teamId) {
+  firebase.database().ref('Team/' + teamId + '/disinfectant').on('value', (snapshot) => {
+    listener(snapshot.val());
+  });
+}
+
+export function registerToiletpaperListener(listener, teamId) {
+  
+  firebase.database().ref('Team/' + teamId).once('value', (snapshot) => {
+    const value = snapshot.val();
+    var members = value.Member.split(",");
+
+    var toiletpaperScores = new Map();
+    for (const index in members) {
+      firebase.database().ref('User/' + members[index] + '/toiletpaper').on('value', (snapshot) => {
+        toiletpaperScores.set(members[index], snapshot.val());
+
+        const sum = (accumulator, currentValue) => accumulator + currentValue;
+        const teamToiletpaperScore = Array.from(toiletpaperScores.values()).reduce(sum);
+        listener(teamToiletpaperScore + 0);
+      });
+    }
+
+  });
+
 }
