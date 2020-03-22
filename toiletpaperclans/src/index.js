a77c2c9fe3decf1081d3e04a058b6a280befc56f
David Boetius <david.boetius@uni-konstanz.de>
2020-03-23 00:34:52 +0100

    Remove testing console log for leaderboard

--- toiletpaperclans/src/index.js
+++ toiletpaperclans/src/index.js
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import Navigator from '_navigations';
 import FirebaseModel from '_utilities/FirebaseModel';
 
-// TODO: for testing
-FirebaseModel.instance().on('leaderboard', b => console.log("board", b));
-
 const App = () => {
   return (
     <Navigator />
