82c5636e42c8ce4cdee5b9216419fe4a668c3e68
cherrywoods <cherrywoods@posteo.org>
2020-03-24 17:50:36 +0100

    Implementing team member not at home overlay

--- toiletpaperclans/src/components/ProfilePicture.js
+++ toiletpaperclans/src/components/ProfilePicture.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { Image, StyleSheet, View, Text } from 'react-native';
 import IsRunningOverlay from '_components/IsRunningOverlay.js';
 
-const ProfilePicture = ({ isRunning, atHome }) => (
-  <View>
-    <Image
-      style={styles.pictureStyle}
-      source={{ uri: 'https://bilder.bild.de/fotos-skaliert/atemschutzmasken-sind-vielerorts-ausverkauft-auch-bei-desinfektionsmitteln-gibt-es-engpaesse-201371552-69076812/6,w=1280,c=0.bild.jpg' }}
-    />
-    <Text>{atHome ? 'At Home' : 'Outside'}</Text>
-    <IsRunningOverlay isRunning={isRunning} />
-  </View>
-);
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const ProfilePicture = ({ isRunning, atHome }) => {
+  const outsideOverlay = atHome ? null : (
+    <View style={styles.outsideOverlay}>
+      <Icon name="run" size={50} color="#F00" />
+    </View>
+  );
+  return (
+    <View>
+      <Image
+        style={styles.pictureStyle}
+        source={{ uri: 'https://bilder.bild.de/fotos-skaliert/atemschutzmasken-sind-vielerorts-ausverkauft-auch-bei-desinfektionsmitteln-gibt-es-engpaesse-201371552-69076812/6,w=1280,c=0.bild.jpg' }}
+      />
+      {outsideOverlay}
+      <IsRunningOverlay isRunning={isRunning} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   pictureStyle: {
@@ -20,6 +29,33 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     margin: 5,
     resizeMode: 'cover',
+  },
+  outsideOverlay: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    margin: 5,
+    backgroundColor: '#B40E2299',
+  },
+  homeOverlay: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    margin: 5,
   },
 });
 
